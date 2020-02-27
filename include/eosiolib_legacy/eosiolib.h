c90e5cd4db4fe75d2a21fb092a90f0bc9ebb16d6
learnforpractice <learnforpractice@gmail.com>
2020-02-27 11:28:11 +0800

    Remove stacktrace.h

--- include/eosiolib_legacy/eosiolib.h
+++ include/eosiolib_legacy/eosiolib.h
@@ -8,18 +8,8 @@
 
 #define EOSIO_THROW(msg) eosio_assert(false, msg)
 
-#ifdef __WASM
-    #define EOSIO_ASSERT(a, b) \
-        eosio_assert(a, b);
-#else
-    #include "stacktrace.h"
-    #define EOSIO_ASSERT(a, b) \
-        if (!(a)) { \
-            print_stacktrace(); \
-        } \
-        eosio_assert(a, b);
-#endif
-
+#define EOSIO_ASSERT(a, b) \
+    eosio_assert(a, b);
 
 #ifndef __WASM
 extern "C" void vmelog_(int line, const char *file, const char *func, const char *fmt, ...);
